40720c92fe44770e9da91810c47cc43b9e64d016
Milind-dev <milinddev1011@gmail.com>
2020-11-25 16:43:51 +0530

    System_Apis_block

--- status/script.js
+++ status/script.js
@@ -8,33 +8,31 @@ function handleForm(){
 				var mailSend = document.createElement('div');
 				mailSend.setAttribute('class','mailSend');
 				mailSend.style.border = "1px solid #0000FF";
-				mailSend.style.width = "120px";
+				mailSend.style.width = "700px";
 				mailSend.style.height = "120px";
 				mailSend.style.marginLeft = "32px";
 					 
 					 var p1p3div = document.createElement('div')
 
 					 var apiV3 = document.createElement('div');
+					 apiV3.style.display= 'flex';
+					 apiV3.style.border= '1px solid green';
+					 
+
 					 var p1 = document.createElement('p');
 					 var p3 = document.createElement('a');
-					 p3.textContent = "operation"
+					 
 					 p1.textContent = "p1";
 					 p1.style.color="red";
 
+					 p3.textContent = "operation"
 					 p3.style.color = "green";
-					 p3.href = "operation";
+					 p3.style.href = "operation";
+					 p3.style.marginLeft="600px"
 					 p3.style.textDecoration = "none"
+
 					 
-					 p
-
-					 // var p2 = document.createElement('p');
-					 // p2.textContent = "p2";
-
-					 // var p3 = document.createElement('p');
-					 // p3.textContent = "p3";
 					 apiV3.append(p1,p3);
-
-					 // apiV3.append(p1,p2,p3);
 			     mailSend.append(apiV3)
 	// plusheader.append(mailSend)
 	insider.append(plusheader,mailSend);
