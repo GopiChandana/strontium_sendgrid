5149b755a0f03118b922eba2122aaaa706a4c21c
Milind-dev <milinddev1011@gmail.com>
2020-11-25 19:17:03 +0530

    status_plus_click_event

--- status/script.js
+++ status/script.js
@@ -1,24 +1,36 @@
 
-
 function handleForm(){
 		event.preventDefault()
 			var insider = document.getElementById('insider')
+			var insider1 = document.getElementById('insider2')
+
 		        var plusheader = document.createElement('div');
 			    plusheader.setAttribute('class','plusheader');
+			    
+			    var plusheader1 = document.createElement('div');
+			    plusheader1.setAttribute('class','plusheader');
+
 				var mailSend = document.createElement('div');
 				mailSend.style.border = "1px solid #0000FF";
 				mailSend.style.width = "690px";
 				mailSend.style.height = "auto";
 				mailSend.style.marginLeft = "32px";
-					 
-					 
+
+				// 	 // change //
+				var mailSend1 = document.createElement('div');
+				mailSend1.style.border = "1px solid #0000FF";
+				mailSend1.style.width = "690px";
+				mailSend1.style.height = "auto";
+				mailSend1.style.marginLeft = "32px";
+
+
 				var apiV5 = document.createElement('div');
 					 apiV5.style.display= 'flex';
 					 apiV5.style.border= '1px solid #294661';
 					 var p1 = document.createElement('p');
 					 var p2 = document.createElement('a');
 					 p1.textContent = "apiV3";
-					 p1.style.color="black";
+					 p1.style.color="color";
 					 // p1.style.fontWeight = "bold"
 					 p2.textContent = "operation"
 					 p2.style.color = "#294661";
@@ -70,72 +82,85 @@ function handleForm(){
 
 					 apiV7.append(p6,p15,p7);
 
+
+
+ 	 // ===================================================
+
+ 	 // second operator
+				var apiV8 = document.createElement('div');
+					 apiV8.style.display= 'flex';
+					 apiV8.style.border= '1px solid #294661';
+					 var p8 = document.createElement('p');
+					 var p9 = document.createElement('a');
+					 p8.textContent = "apiV3";
+					 p8.style.color="black";
+					 // p8.style.fontWeight = "bold"
+					 p9.textContent = "operation"
+					 p9.style.color = "#294661";
+					 p9.style.href = "operation";
+					 p9.style.marginLeft="530px"
+					 p9.style.textDecoration = "none"
+
+					 apiV8.append(p8,p9);
+
+
+
+				var apiV9 = document.createElement('div');
+					 apiV9.style.display= 'flex';
+					 apiV9.style.border= '1px solid #294661';
+					 
+					 var p10 = document.createElement('p');
+					 var p11 = document.createElement('a');
+					 p10.textContent = "apiV3";
+					 p10.style.color="black";
+					 // p10.style.fontWeight = "bold"
+					 p11.textContent = "operation"
+					 p11.style.color = "#294661";
+					 p11.style.href = "operation";
+					 p11.style.marginLeft="530px"
+					 p11.style.textDecoration = "none"
+					 apiV9.append(p10,p11);
+
+				var apiV10 = document.createElement('div');
+					 apiV10.style.display= 'flex';
+					 apiV10.style.border= '1px solid #294661';
+					 var p12 = document.createElement('p');
+					 var p13 = document.createElement('a');
+					 p12.textContent = "apiV3";
+					 p12.style.color="black";
+					 // p12.style.fontWeight = "bold"
+					 p13.textContent = "operation"
+					 p13.style.color = "#294661";
+					 p13.style.href = "operation";
+					 p13.style.marginLeft="530px"
+					 p13.style.textDecoration = "none"
+					 apiV10.append(p12,p13);
+
+
+					mailSend.append(apiV5,apiV6,apiV7)
+					mailSend1.append(apiV8,apiV9,apiV10) 
+					
+
+			// plusheader.append(mailSend1)
+
 	     // mailSend.append(apiV5,apiV6,apiV7)
 	     // mailSend.append(apiV8,apiV9,apiV10)
 		// mailSend.append(apiV5,apiV6,apiV7,apiV8,apiV9,apiV10)
-				mailSend.append(apiV5,apiV6,apiV7)
-	// plusheader.append(mailSend)
+		
+	 // plusheader.append(mailSend)
 	insider.append(plusheader,mailSend);
+	insider2.append(plusheader1,mailSend1);
 }
+
 
 window.addEventListener('load',function(){
 	var btn = document.getElementById('plus');
 	btn.addEventListener('click',handleForm)
+	var btn1 = document.getElementById('plus2');
+	btn1.addEventListener('click',handleForm)
 })
 
 
 
 
 
- 	 
-// 				var apiV8 = document.createElement('div');
-// 					 apiV8.style.display= 'flex';
-// 					 apiV8.style.border= '1px solid green';
-// 					 var p8 = document.createElement('p');
-// 					 var p9 = document.createElement('a');
-// 					 p8.textContent = "apiV3";
-// 					 p8.style.color="red";
-// 					 p8.style.fontWeight = "bold"
-// 					 p9.textContent = "operation"
-// 					 p9.style.color = "green";
-// 					 p9.style.href = "operation";
-// 					 p9.style.marginLeft="600px"
-// 					 p9.style.textDecoration = "none"
-
-// 					 apiV8.append(p8,p9);
-
-
-
-// 				var apiV9 = document.createElement('div');
-// 					 apiV9.style.display= 'flex';
-// 					 apiV9.style.border= '1px solid green';
-					 
-// 					 var p10 = document.createElement('p');
-// 					 var p11 = document.createElement('a');
-// 					 p10.textContent = "apiV3";
-// 					 p10.style.color="black";
-// 					 p10.style.fontWeight = "bold"
-// 					 p11.textContent = "operation"
-// 					 p11.style.color = "green";
-// 					 p11.style.href = "operation";
-// 					 p11.style.marginLeft="600px"
-// 					 p11.style.textDecoration = "none"
-// 					 apiV9.append(p4,p5);
-
-// 				var apiV10 = document.createElement('div');
-// 					 apiV10.style.display= 'flex';
-// 					 apiV10.style.border= '1px solid green';
-// 					 var p12 = document.createElement('p');
-// 					 var p13 = document.createElement('a');
-// 					 p12.textContent = "apiV3";
-// 					 p12.style.color="red";
-// 					 p12.style.fontWeight = "bold"
-// 					 p13.textContent = "operation"
-// 					 p13.style.color = "green";
-// 					 p13.style.href = "operation";
-// 					 p13.style.marginLeft="600px"
-// 					 p13.style.textDecoration = "none"
-// 					 apiV10.append(p6,p7);
-
-
-// mailSend.append(apiV8,apiV9,apiV10)
