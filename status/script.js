f8fbe198ffa87f00d254e9bb4fe99d556b1e4cfb
Milind-dev <milinddev1011@gmail.com>
2020-11-25 15:35:30 +0530

    status_Dom_SystemOperation

--- status/script.js
+++ status/script.js
@@ -1,7 +1,17 @@
 
 
 function handleForm(){
-	
+	event.preventDefault()
+	var insider = document.getElementById('insider')
+	var plusheader = document.createElement('div');
+	plusheader.setAttribute('class','plusheader');
+	var mailSend = document.createElement('div');
+	mailSend.setAttribute('class','mailSend');
+	mailSend.style.border = "1px solid #0000FF";
+	mailSend.style.width = "120px";
+	mailSend.style.height = "120px";
+	// plusheader.append(mailSend)
+	insider.append(plusheader,mailSend);
 }
 
 window.addEventListener('load',function(){
